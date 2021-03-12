36e77c15e8f475112d79e2ae40e152101999be5a
Julian Pettit <julian.s.pettit@gmail.com>
2021-03-12 18:18:28 -0500

    add compound interest

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
-def simple_interest(principle, rate, period):
+def simple_interest(principle, rate_per_period, periods):
     interest = rate * period
     return principle * (1 + interest)
+
+def compount_interest(principle, rate_per_period, periods, compoundings_per_period):
+    effective_rate = rate_per_period / compoundings_per_period
+    total_periods = periods * compoundings_per_period
+    return principle * ((1 + effective_rate) ** total_periods)
 
