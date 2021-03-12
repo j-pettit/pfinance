c20462b733752fdd21e727afe487f02678cf52df
Julian Pettit <julian.s.pettit@gmail.com>
2021-03-12 18:22:36 -0500

    add tests and fix functions

--- functions.py
+++ functions.py
@@ -1,9 +1,12 @@
 def simple_interest(principle, rate_per_period, periods):
-    interest = rate * period
+    interest = rate_per_period * periods
     return principle * (1 + interest)
 
-def compount_interest(principle, rate_per_period, periods, compoundings_per_period):
+def compound_interest(principle, rate_per_period, periods, compoundings_per_period):
     effective_rate = rate_per_period / compoundings_per_period
     total_periods = periods * compoundings_per_period
     return principle * ((1 + effective_rate) ** total_periods)
 
+
+print(simple_interest(100, 0.10, 10))
+print(compound_interest(100, 0.10, 10, 12))
