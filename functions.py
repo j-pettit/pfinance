1635dcf7da9bbc5d351f5c39f838dcc29533e285
Julian Pettit <julian.s.pettit@gmail.com>
2021-03-13 20:11:28 -0500

    add tests

--- functions.py
+++ functions.py
@@ -16,5 +16,6 @@ def future_value_series(payment, rate_per_period, periods, compoundings_per_peri
     return payment * ((((1 + effective_rate) ** total_periods) - 1) / (effective_rate)) * start_of_period_modifier
 
 print('run tests')
-print(simple_interest(100, 0.10, 10), '200')
-print(compound_interest(100, 0.10, 10, 12), '270.70')
+print(f'{simple_interest(100, 0.10, 10):.2f} : 200.00')
+print(f'{compound_interest(100, 0.10, 10, 12):.2f} : 270.70')
+print(f'{future_value_series(100, 0.05, 10, 12):.2f} : 15528.23')
