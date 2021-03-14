36fc9a181b9475ae889c1cf706d41d05f36e7a19
Julian Pettit <julian.s.pettit@gmail.com>
2021-03-13 20:14:12 -0500

    add name guard

--- functions.py
+++ functions.py
@@ -15,8 +15,9 @@ def future_value_series(payment, rate_per_period, periods, compoundings_per_peri
         start_of_period_modifier = 1 + effective_rate
     return payment * ((((1 + effective_rate) ** total_periods) - 1) / (effective_rate)) * start_of_period_modifier
 
-print('run tests')
-print(f'{simple_interest(100, 0.10, 10):.2f} : 200.00')
-print(f'{compound_interest(100, 0.10, 10, 12):.2f} : 270.70')
-print(f'{future_value_series(100, 0.05, 10, 12):.2f} : 15528.23')
-print(f'{future_value_series(100, 0.05, 10, 12, True):.2f} : 15592.93')
+if __name__ == '__main__':
+    print('run tests')
+    print(f'{simple_interest(100, 0.10, 10):.2f} : 200.00')
+    print(f'{compound_interest(100, 0.10, 10, 12):.2f} : 270.70')
+    print(f'{future_value_series(100, 0.05, 10, 12):.2f} : 15528.23')
+    print(f'{future_value_series(100, 0.05, 10, 12, True):.2f} : 15592.93')
