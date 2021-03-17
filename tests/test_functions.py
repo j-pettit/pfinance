da05ee8d90f781ad3e8a58e98d1ff4f1d28b0c30
Julian Pettit <julian.s.pettit@gmail.com>
2021-03-17 19:23:52 -0400

    update test functions

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -4,3 +4,8 @@
 #     print(f'{compound_interest(100, 0.10, 10, 12):.2f} : 270.70')
 #     print(f'{future_value_series(100, 0.05, 10, 12):.2f} : 15528.23')
 #     print(f'{future_value_series(100, 0.05, 10, 12, True):.2f} : 15592.93')
+
+from py-finance import functions # this doesn't work, modules with -?
+
+def test_simple_interest():
+    assert functions.simple_interest(100, 0.10, 10) == 200.0
