839cf2b108616e29dfb00baef74a775065d2b984
Julian Pettit <julian.s.pettit@gmail.com>
2021-03-26 21:59:52 -0400

    only publish on push to master

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='pfinance',
     packages=find_packages(include=['pfinance']),
-    version='0.0.0',
+    version='0.0.1',
     description='Financial mathematics library',
     author='Julian Pettit',
     license='MIT',
