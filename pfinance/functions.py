da1505246c0fb120183f00db0c687af2fa169f96
Julian Pettit <julian.s.pettit@gmail.com>
2021-03-28 15:52:36 -0400

    update interest functions

--- pfinance/functions.py
+++ pfinance/functions.py
@@ -4,29 +4,60 @@ def simple_interest(principle: float, interest_rate: float, periods: int) -> flo
 
         Parameters:
             principle (float): Present value of the investment
-            interest_rate (float): The interest rate per investment period, e.g. year
-            periods (int): The term of the investment, e.g. years
+            interest_rate (float): Interest rate per period, e.g. year
+            periods (int): Term of the investment, e.g. years
 
         Returns:
-            future_value (float): The value of the investment after the term
+            future_value (float): Value of the investment after the term
     '''
     interest = interest_rate * periods
     return principle * (1 + interest)
 
 
-def compound_interest(principle: float, rate_per_period: float, periods: int, compoundings_per_period: int) -> float:
-    effective_rate = rate_per_period / compoundings_per_period
-    total_periods = periods * compoundings_per_period
+def compound_interest(principle: float, interest_rate: float, periods: int, compounding_frequency: int = 1) -> float:
+    '''
+    Returns the total value of an investment earning compound interest.
+
+        Parameters:
+            principle (float): Present value of the investment
+            interest_rate (float): Interest rate per period, e.g. year
+            periods (int): Term of the investment, e.g. years
+            compounding_frequency (int): Number of compoundings that occur period, default 1
+
+        Returns:
+            future_value (float): Value of the investment after the term
+    '''
+    effective_rate = interest_rate / compounding_frequency
+    total_periods = periods * compounding_frequency
     return principle * ((1 + effective_rate) ** total_periods)
 
 
-def future_value_series(payment, rate_per_period, periods, compoundings_per_period, start_of_period=False):
-    effective_rate = rate_per_period / compoundings_per_period
-    total_periods = periods * compoundings_per_period
-    start_of_period_modifier = 1
+def future_value_series(
+    payment: float,
+    interest_rate: float,
+    periods: int,
+    compounding_frequency: int = 1,
+    start_of_period: bool = False,
+) -> float:
+    '''
+    Returns the total value of an future value series earning compound interest with regular additions.
+
+        Parameters:
+            payment (float): Value of the regular additions
+            interest_rate (float): Interest rate per period, e.g. year
+            periods (int): Number of additions and term of the investment, e.g. years
+            compounding_frequency (int): Number of compoundings that occur period, default 1
+            start_of_period (bool): Make the payment at the start of each period, default False
+
+        Returns:
+            future_value (float): Value of the investment after the term
+    '''
+    effective_rate = interest_rate / compounding_frequency
+    total_periods = periods * compounding_frequency
+    start_modifier = 1
     if start_of_period:
-        start_of_period_modifier = 1 + effective_rate
-    return payment * ((((1 + effective_rate) ** total_periods) - 1) / (effective_rate)) * start_of_period_modifier
+        start_modifier = 1 + effective_rate
+    return payment * ((((1 + effective_rate) ** total_periods) - 1) / (effective_rate)) * start_modifier
 
 
 def calculate_acb(transactions):
