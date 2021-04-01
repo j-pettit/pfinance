8af545403f339fec261f0c3965d91881f528a5bd
Julian Pettit <julian.s.pettit@gmail.com>
2021-03-31 21:39:17 -0400

    update loan payment function

--- pfinance/functions.py
+++ pfinance/functions.py
@@ -22,7 +22,7 @@ def compound_interest(principle: float, interest_rate: float, periods: int, comp
             principle (float): Present value of the investment
             interest_rate (float): Interest rate per period, e.g. year
             periods (int): Term of the investment, e.g. years
-            compounding_frequency (int): Number of compoundings that occur period, default 1
+            compounding_frequency (int): Number of compoundings that occur per period, default 1
 
         Returns:
             future_value (float): Value of the investment after the term
@@ -46,7 +46,7 @@ def future_value_series(
             payment (float): Value of the regular additions
             interest_rate (float): Interest rate per period, e.g. year
             periods (int): Number of additions and term of the investment, e.g. years
-            compounding_frequency (int): Number of compoundings that occur period, default 1
+            compounding_frequency (int): Number of compoundings that occur per period, default 1
             start_of_period (bool): Make the payment at the start of each period, default False
 
         Returns:
@@ -60,7 +60,29 @@ def future_value_series(
     start_modifier = 1
     if start_of_period:
         start_modifier = 1 + effective_rate
-    return payment * ((((1 + effective_rate) ** total_periods) - 1) / (effective_rate)) * start_modifier
+    return payment * start_modifier * (((1 + effective_rate) ** total_periods) - 1) / effective_rate
+
+
+def loan_payment(principal: float, interest_rate: float, payment_frequency: int, term: int, down_payment: float = 0) -> float:
+    '''
+    Returns the periodic payment required to repay a loan accruing compound interest.
+
+        Parameters:
+            principal (float): Initial value of the loan
+            interest_rate (float): Interest rate per period, e.g. year
+            payment_frequency (int): Number of payments and compoundings per period, e.g. year
+            term (int): Term of the loan in number of payments
+            down_payment (float): Amount paid towards the loan before interest, default 0
+
+        Returns:
+            periodic_payment (float): Period loan payment
+    '''
+    loan_amount = principal - down_payment
+    if interest_rate == 0:
+        return loan_amount / term
+
+    effective_rate = interest_rate / payment_frequency
+    return loan_amount * effective_rate * (1 + effective_rate) ** term / ((1 + effective_rate) ** term - 1)
 
 
 def calculate_acb(transactions):
@@ -75,12 +97,3 @@ def calculate_acb(transactions):
             total_shares += shares
             book_value += shares * price
     return book_value / total_shares
-
-
-def auto_loan_monthly_payment(vehicle_price, down_payment, interest_apr_percent, loan_term_months):
-    # Interest compounded and applied monthly.
-    interest_apr = interest_apr_percent / 100
-    loan_amount = vehicle_price - down_payment
-    numerator = loan_amount * (interest_apr/12) * (1 + interest_apr/12)**loan_term_months
-    denominator = (1 + interest_apr/12)**loan_term_months - 1
-    return round(numerator / denominator, 2)
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,10 +1,3 @@
-# test = calculate_acb([(100, 10, 10), (-50, 10, 10)])
-#     # Auto loan monthly payment tests.
-#     print('Auto loan monthly payment tests')
-#     print(func.auto_loan_monthly_payment(10000, 0, 10, 3), ': 3389.04')
-#     print(func.auto_loan_monthly_payment(21431, 4147, 1.92, 48), ': 374.38')
-#     print(func.auto_loan_monthly_payment(44065, 12789, 2.38, 60), ': 553.41')
-
 from pfinance import functions
 
 
@@ -22,3 +15,9 @@ def test_future_value_series():
     assert functions.future_value_series(100, 0, 10) == 1000.00
     assert round(functions.future_value_series(100, 0.05, 10, 12), 2) == 15528.23
     assert round(functions.future_value_series(100, 0.05, 10, 12, True), 2) == 15592.93
+
+
+def test_loan_payment():
+    assert functions.loan_payment(1000, 0, 1, 10) == 100.00
+    assert round(functions.loan_payment(100000, 0.10, 12, 60), 2) == 2124.70
+    assert round(functions.loan_payment(150000, 0.10, 12, 60, 50000), 2) == 2124.70
